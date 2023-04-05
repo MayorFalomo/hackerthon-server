2c2f1866373d83ebebaf001d862f67bca085c9cd
MayorFalomo <mayorfalomo@gmail.com>
2023-04-05 22:36:45 +0100

    header in index

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const userRoutes = require("./routes/users");
 
 dotEnv.config({ path: "./vars/.env" });
 app.use(express.json());
-app.use(cors());
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // Update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URL, {
